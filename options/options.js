3c59756d13c8d4325edfc271f93954475c8b2838
Crowdbotics <engineering@crowdbotics.com>
2023-03-28 06:07:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-sound-dev-72576",
+  url: "https://lucky-sound-dev-72576.botics.co",
+  api: "https://lucky-sound-dev-72576.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
